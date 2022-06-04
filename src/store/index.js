410045b8795c05cd7bcfa5b772b45484efa2dd30
cihatsolak <cihatsolak@hotmail.com>
2022-06-05 01:27:57 +0300

    Redux persist integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
 import todoReducer from './reducers/todoReducer'
 import { combineReducers, createStore } from 'redux'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
 
-const reducer = combineReducers({
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+    whiteList: ["count"], //count reducer'ı hiçbir zaman kaybolmayacak. local storage tutulacak.
+    blackList: ["user"] //user reducer'ı local storage'da tutma. sayfa yenilendiğinde kaybolsun 
+}
+
+const reducers = combineReducers({
     todo: todoReducer
 })
 
-export const store = createStore(reducer)
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(
+    persistedReducer
+)
+
+export const persistor = persistStore(store)
+
+export default reducers
