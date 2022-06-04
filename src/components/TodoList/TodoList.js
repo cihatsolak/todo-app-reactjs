0ed9270fa80c16057def23d7cba063e7d436a8b3
cihatsolak <cihatsolak@hotmail.com>
2022-06-04 17:29:11 +0300

    Correct the style of the delete button

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import './TodoList.css'
 import TodoItem from '../TodoItem/TodoItem'
 
-const TodoList = (props) => {
+const TodoList = () => {
     return (
         <div className='item-content-wrapper'>
             <div className='content'>
